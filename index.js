792b5bcca210d6b8f9b0c9bfadacf5b646ba6a1f
ngeTaco <thealdo.531@gmail.com>
2024-05-16 21:25:26 -0600

    added timeout to completed messaging

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+function completeTodo(event) {
+    const value = event.target.getAttribute("aria-checked");
+    const message = document.querySelector("aside");
+    if (value !== 'true') {
+        event.target.setAttribute("aria-checked", "true");
+        message.style.visibility = "visible";
+        setTimeout(() => {
+            message.removeAttribute("style");
+        }, 2000);
+    } else {
+        event.target.setAttribute("aria-checked", "false");
+    }
+};
+
 function removeTodo(event) {
+    event.stopPropagation();
     event.target.parentNode.remove();
-}
+}//event stoppropagation
 
 function addTodo(event) {
     event.preventDefault();
@@ -18,12 +33,3 @@ function addTodo(event) {
 }
 
 document.querySelector("form").addEventListener("submit", addTodo);
-
-function completeTodo(event) {
-    const value = event.target.getAttribute("aria-checked");
-    if (value !== 'true') {
-        event.target.setAttribute("aria-checked", "true");
-    } else {
-        event.target.setAttribute("aria-checked", "false");
-    }
-};
