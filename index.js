1873486ba755f1113ec644bd5b9e4b74d65d516d
ngeTaco <thealdo.531@gmail.com>
2024-05-16 19:43:45 -0600

    Step 2 Complete. Remove todo item fn added

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+function removeTodo(event) {
+    event.target.parentNode.remove();
+}
+
 function addTodo(event) {
     event.preventDefault();
     const item = document.createElement("li");
     item.innerText = document.querySelector("#item").value;
+    
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", removeTodo);
+    item.appendChild(button);
 
     const list = document.querySelector("ul");
     list.appendChild(item);
